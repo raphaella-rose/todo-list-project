e8a81846de1a4ec52a544cc79d1038ed89e7711d
raphaella-rose <raphaella.rose1@gmail.com>
2021-09-07 11:47:54 +0100

    created filter function for completed tasks

--- src/App.js
+++ src/App.js
@@ -7,18 +7,22 @@ import './App.css';
 
 function App() {
 const [toDoList, setToDoList] = useState(data);
-
 const handleToggle = (id) => {
 let mapped = toDoList.map(task => {
 return task.id === Number(id) ? {...task, complete: !task.complete} : {...task};
 });
 setToDoList(mapped);
 }
-
+const filterTasks = () => {
+let filtered = toDoList.filter(task => {
+return !task.complete;
+});
+setToDoList(filtered);
+}
   return (
     <div className="App">
     <Header />
-    <ToDoList handleToggle={handleToggle} toDoList={toDoList} />
+    <ToDoList filterTasks={filterTasks} handleToggle={handleToggle} toDoList={toDoList} />
     </div>
   );
 }
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ToDo from "./ToDo";
 
-const ToDoList = ({toDoList, handleToggle}) => {
-  
+const ToDoList = ({toDoList, handleToggle, filterTasks}) => {
+  const handleClick = (e) => {
+  e.preventDefault();
+  filterTasks();
+  }
   return (
     <div>
       {toDoList.map((todo) => {
         return <ToDo key={todo.id} todo={todo} handleToggle={handleToggle} />
       })}
+      <button onClick={handleClick}>Clear Completed Tasks</button>
     </div>
   )
 }
